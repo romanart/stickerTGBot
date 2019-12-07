c7816031cab98628bb76d6c957bc5010c9c1c65e
Roman Artemev <roman.artemev.92@gmail.com>
2019-12-08 00:22:54 +0300

    Add sql script to create database

---
+++ database.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE IF NOT EXISTS stickerBotDataBase;
+
+USE stickerBotDataBase;
+
+CREATE TABLE IF NOT EXISTS userState (
+    user_id   BIGINT   NOT NULL,
+    chat_id   BIGINT   NOT NULL,
+    state_id  SMALLINT NOT NULL,
+    PRIMARY KEY (user_id, chat_id)
+);
+
+CREATE TABLE IF NOT EXISTS userCurrentStickerPack (
+    user_id   BIGINT   NOT NULL,
+    chat_id   BIGINT   NOT NULL,
+    sticker_pack_id  VARCHAR(1000) NOT NULL,
+    PRIMARY KEY (user_id, chat_id)
+);
+
+CREATE TABLE IF NOT EXISTS userStickerPacks (
+    user_id   BIGINT   NOT NULL,
+    chat_id   BIGINT   NOT NULL,
+    sticker_pack_id  VARCHAR(1000) NOT NULL,
+    PRIMARY KEY (user_id, chat_id)
+);
+
+CREATE TABLE IF NOT EXISTS defaultUserEmojy (
+    user_id   BIGINT   NOT NULL UNIQUE,
+    emojy  VARCHAR(1) CHARACTER SET utf8mb4 NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS chatWelcomeMessage (
+    chat_id   BIGINT   NOT NULL UNIQUE ,
+    welcome_message  VARCHAR(1000) NOT NULL,
+    PRIMARY KEY (chat_id)
+);
+
+CREATE TABLE IF NOT EXISTS chatUserCaption (
+    chat_id   BIGINT   NOT NULL,
+    user_id   BIGINT   NOT NULL,
+    welcome_message  VARCHAR(1000) NOT NULL,
+    CONSTRAINT uniqChatUser UNIQUE (chat_id, user_id)
+);
