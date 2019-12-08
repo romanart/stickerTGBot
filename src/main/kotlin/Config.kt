704af4e2e2b88c6d7d307fe139576366282d4674
Roman Artemev <roman.artemev.92@gmail.com>
2019-12-09 01:49:39 +0300

    Add database configuration

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -3,7 +3,10 @@ import kotlinx.serialization.json.*
 import java.io.File
 
 @Serializable
-class Config(val botName: String, val botToken: String, val ownerID: Long)
+class DatabaseConfig(val host: String, val port: Int, val region: String, val databaseName: String, val type: String, val user: String, val password: String?, val certificate: String)
+
+@Serializable
+class Config(val botName: String, val botToken: String, val ownerID: Long, val databaseConfig: DatabaseConfig)
 
 fun parseConfig(file: File): Config {
     val json = Json(JsonConfiguration.Stable)
