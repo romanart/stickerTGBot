08c7b4412888f7fd764fc080d6f51115924d7c2a
Roman Artemev <roman.artemev.92@gmail.com>
2019-12-15 01:19:11 +0300

    Add stop command

--- src/main/kotlin/command/StopCommand.kt
+++ src/main/kotlin/command/StopCommand.kt
@@ -2,10 +2,14 @@ package command
 
 import StickerBot
 import org.telegram.telegrambots.meta.api.objects.Message
+import kotlin.system.exitProcess
 
 class StopCommand(private val ownerId: Long) : TextCommand("/stop", true, "Stop the bot") {
     override fun execute(message: Message, botAPI: StickerBot): String? {
-        if (ownerId == message.chatId) botAPI.onClosing()
+        if (ownerId == message.chatId) {
+            botAPI.onClosing()
+            exitProcess(0)
+        }
         return null
     }
 }
